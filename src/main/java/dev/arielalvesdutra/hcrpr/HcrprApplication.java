e31cfe0641dfa924610202a2959c17d944a35955
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-11-13 09:41:26 -0300

    Commit inicial

---
+++ src/main/java/dev/arielalvesdutra/hcrpr/HcrprApplication.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.hcrpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcrprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcrprApplication.class, args);
+	}
+
+}
